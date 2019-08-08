b93d217ab05a6d18f2ff80b1fcfdfed9489c5327
David Acosta <davidacosta25081@yahoo.com>
2019-08-08 19:13:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-get 'articles/:id/edit' , to: 'articles#edit' ,  as: :edit_article 
+get 'articles/:id/edit' , to: 'articles#edit' ,  as: :edit_article
 patch 'articles/:id' , to 'articles#update'
 
 end
